195d40b8cfee21abf9a01e1580666cd053aca169
CARLOS BENDEK <cabendek@uc.cl>
2021-09-03 14:01:21 -0400

    update

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -28,6 +28,44 @@ int main(int argc, char const *argv[])
   }
   printf("\n");
 
+  
+  //-- AQUI EMPIEZA --
+ 
+  char* distancia_semaforo1_s = data_in->lines[0][0];
+  char* distancia_semaforo2_s = data_in->lines[0][1];
+  char* distancia_semaforo3_s = data_in->lines[0][2];
+  char* distancia_bodega_s = data_in->lines[0][3];
+
+  char* tiempo_de_creacion_s = data_in->lines[1][0];
+  char* envios_necesarios_s = data_in->lines[1][1];
+  char* tiempo_1_s = data_in->lines[1][2];
+  char* tiempo_2_s = data_in->lines[1][3];
+  char* tiempo_3_s = data_in->lines[1][4];
+  
+  int distancia_semaforo1 = atoi(distancia_semaforo1_s);
+  int distancia_semaforo2 = atoi(distancia_semaforo2_s);
+  int distancia_semaforo3 = atoi(distancia_semaforo3_s);
+  int distancia_bodega = atoi(distancia_bodega_s);
+
+  int tiempo_de_creacion = atoi(tiempo_de_creacion_s);
+  int envios_necesarios = atoi(envios_necesarios_s);
+  int tiempo_1 = atoi(tiempo_1_s);
+  int tiempo_2 = atoi(tiempo_2_s);
+  int tiempo_3 = atoi(tiempo_3_s);
+
+  printf("%i, %i, %i, %i\n",distancia_semaforo1, distancia_semaforo2, distancia_semaforo3, distancia_bodega);
+  printf("%i, %i, %i, %i, %i\n",tiempo_de_creacion, envios_necesarios, tiempo_1, tiempo_2, tiempo_3);
+/*
+  int fabrica = fork();
+  if (!fabrica){
+    for (int i = 0; i < 3; i++){
+      repartidores = fork()
+    };
+  }
+  int semaforo1 = fork();
+  int semaforo2 = fork();
+  int semaforo3 = fork();
+*/
   printf("Liberando memoria...\n");
   input_file_destroy(data_in);
 }
