2e26cbe1f02584b659fb0f932ebaa81b3a5980bd
cabendek <48304100+cabendek@users.noreply.github.com>
2021-09-05 22:13:40 -0300

    Solution repartidor (#1)
    * new idea
    
    * me cambio de compu
    
    * Co-authored-by: cabendek <cabendek@users.noreply.github.com>
    
    Co-authored-by: TamaraErdmann <taerdmann@uc.cl>

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -8,7 +8,40 @@
 #include "../file_manager/manager.h"
 
 int estado_semaforos[3] = {1,1,1};
+int tiempo_repartidores;
+int cant_repartidores;
+int contador = 1;
+int distancia_semaforo1;
+int distancia_semaforo2;
+int distancia_semaforo3;
+int distancia_bodega;
 
+void handle_sigalrm(int sig) {
+  // Vamos a enviarle todos los números al hijo
+
+  int repartidor = fork();
+
+  if (!repartidor){
+    char distancia_1_s[10];
+    char distancia_2_s[10];
+    char distancia_3_s[10];
+    char distancia_4_s[10];
+    sprintf(distancia_1_s,"%d", distancia_semaforo1);
+    sprintf(distancia_2_s,"%d", distancia_semaforo2);
+    sprintf(distancia_3_s,"%d", distancia_semaforo3);
+    sprintf(distancia_4_s,"%d", distancia_bodega);
+
+    char* args[] = {distancia_1_s,distancia_2_s,distancia_3_s,distancia_4_s,NULL};
+    printf("Cree un repartidor\n");
+    if(execv("repartidor", args) == -1) {
+      printf("\nfailed connection\n");
+    }
+  };
+  if (contador < cant_repartidores) {
+    alarm(tiempo_repartidores);
+    contador += 1;
+  }
+};
 
 int cambio_semaforo (int semaforo){
   if (estado_semaforos[semaforo] == 0){
@@ -22,10 +55,26 @@ int cambio_semaforo (int semaforo){
   return 0;
 };
 
-void print(int sig, siginfo_t *siginfo, void *context) {
+// POR MODIFICAR, MANEJO DE 2 RECEPCIONES DE SEÑAL
+void handle_siguser1(int sig, siginfo_t *siginfo, void *context) {
   int number_received = siginfo->si_value.sival_int;
-  printf("\n\t -----> CTMMMM LLEGO Y RECIBI EL SEMAFORO: %i\n", number_received);
-}
+  printf("> > > > Mensaje recibido: %d\n",number_received );
+  int from = (number_received/10%10);
+  int id_semaforo = (number_received%10);
+  
+  if (from == 1) { //SEMAFORO
+    if (estado_semaforos[id_semaforo] == 0) {
+      estado_semaforos[id_semaforo] = 1;
+    } else {
+      estado_semaforos[id_semaforo] = 0;
+    }
+  } else if (from == 2) { //REPARTIDOR
+    int PID_repartidor = (number_received/100);
+    send_signal_with_int(PID_repartidor, estado_semaforos[id_semaforo]);
+  } else {
+    printf("Esto NO se deberia imprimir");
+  }
+};
 
 /*
 void consultar_semaforo (int semaforo, int PID){
@@ -76,16 +125,19 @@ int main(int argc, char const *argv[])
   printf("Liberando memoria...\n");
   input_file_destroy(data_in);
   
-  int distancia_semaforo1 = linea_1[0];
-  int distancia_semaforo2 = linea_1[1];
-  int distancia_semaforo3 = linea_1[2];
-  int distancia_bodega = linea_1[3];
+  distancia_semaforo1 = linea_1[0];
+  distancia_semaforo2 = linea_1[1];
+  distancia_semaforo3 = linea_1[2];
+  distancia_bodega = linea_1[3];
 
   int tiempo_de_creacion = linea_2[0];
   int envios_necesarios = linea_2[1];
   int tiempo_1 = linea_2[2];
   int tiempo_2 = linea_2[3];
   int tiempo_3 = linea_2[4];
+
+  tiempo_repartidores = tiempo_de_creacion;
+  cant_repartidores = envios_necesarios;
 
   // Printeamos variables (para verificarlas)
   // printf("%d, %d, %d, %d\n",distancia_semaforo1, distancia_semaforo2, distancia_semaforo3, distancia_bodega);
@@ -104,46 +156,21 @@ int main(int argc, char const *argv[])
     //Creamos el camino
     
 
+
     // Fabrica crea a repartidores cada tiempo_de_creacion segundos:
+    
     /* REPARTIDORES */
 
-    connect_sigaction(SIGUSR1,print);
+    connect_sigaction(SIGUSR1, handle_siguser1);
+    signal(SIGALRM, handle_sigalrm);
+    
+    alarm(tiempo_de_creacion);
+    // free(camino);
 
-    for (int i = 0; i < envios_necesarios; i++) { 
-      
-      sleep(tiempo_de_creacion);
-
-      printf("**TIEMPO DE ESPERA: %d SEG\n",tiempo_de_creacion);
-      int repartidor = fork();
-
-      if (!repartidor){
-  
-        printf("---ENTREE AQUI----\n");
-        // transformamos a char* cada valor bajo el supuesto de que no habran numeros con mas de 30 digitos
-        
-        // Semaforo 1
-        char d_semaforo1_s[30];
-        sprintf(d_semaforo1_s, "%d", distancia_semaforo1);
-        // Semaforo 2
-        char d_semaforo2_s[30];
-        sprintf(d_semaforo2_s, "%d", distancia_semaforo2);
-        // Semaforo 3
-        char d_semaforo3_s[30];
-        sprintf(d_semaforo3_s, "%d", distancia_semaforo3);
-        // Bodega
-        char d_bodega_s[30];
-        sprintf(d_bodega_s, "%d", distancia_bodega);
-        
-        char* args[] = {d_semaforo1_s, d_semaforo2_s, d_semaforo3_s, d_bodega_s, NULL};
-
-        if(execv("repartidor", args) == -1) {
-          printf("\nfailed connection\n");
-        }
-      }
-    };
     while(true);
     printf("\n\tEsto NO se deberia imprimir\n");
     wait(NULL); //Manejo de finalizacion
+    
 
   } else {
 
@@ -156,7 +183,7 @@ int main(int argc, char const *argv[])
       char tiempo_1_s[30];
       sprintf(tiempo_1_s,"%d", tiempo_1);
       
-      char* argf[] = {tiempo_1_s,fabrica_s,NULL};
+      char* argf[] = {tiempo_1_s,fabrica_s,"0",NULL};
       if(execv("semaforo", argf) == -1){
         printf("\nFallo la conexion del 1er semaforo\n");
       };
@@ -167,7 +194,7 @@ int main(int argc, char const *argv[])
         char tiempo_2_s[30];
         sprintf(tiempo_2_s,"%d", tiempo_2);
 
-        char* argf[] = {tiempo_2_s,fabrica_s,NULL};
+        char* argf[] = {tiempo_2_s,fabrica_s,"1",NULL};
         if(execv("semaforo", argf) == -1){
           printf("\nFallo la conexion del 2do semaforo\n");
         };
@@ -178,7 +205,7 @@ int main(int argc, char const *argv[])
             char tiempo_3_s[30];
             sprintf(tiempo_3_s,"%d", tiempo_3);
 
-            char* argf[] = {tiempo_3_s,fabrica_s,NULL};
+            char* argf[] = {tiempo_3_s,fabrica_s,"2",NULL};
             if(execv("semaforo", argf) == -1){
               printf("\nFallo la conexion del 3er semaforo\n");
             };
--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -4,100 +4,94 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-// funcion para crear el array - camino
-// int* crear_camino(int distancia_semaforo1, int distancia_semaforo2, int distancia_semaforo3, int distancia_bodega)
-// {
-//   int* camino = calloc(distancia_bodega, sizeof(int));
-//   for (int i = 0, i <= distancia_bodega, i++)
-//   {
-//     if ((i = distancia_semaforo1) || (i = distancia_semaforo2) || (i = distancia_semaforo3)){
-//       camino[i] = 1;
-//     }
-//     else if (i = distancia_bodega)
-//     {
-//     camino[i] = 2;
-//     }
-//   }
-//   return camino;
-// }
+#include "../file_manager/manager.h"
 
-/*
-int* camino = calloc(distancia_bodega, sizeof(int));
-for (int i = 0, i <= distancia_bodega, i++){
-  if ((i = distancia_semaforo1) || (i = distancia_semaforo2) || (i = distancia_semaforo3)){
-    camino[i] = 1;
-  };
-  else if (i = distancia_bodega){
-    camino[i] = 2;
-  };
-};
+int semaforo_cruzado = 0;
+int contador_tiempos[4] = {0, 0, 0, 0};
+int tiempo = 0;
+int posicion_actual = 0;
 
-int luz_semaforo(int sig, siginfo_t *siginfo, void *context){
+void luz_semaforo(int sig, siginfo_t *siginfo, void *context){
   int number_received = siginfo->si_value.sival_int;
-  return number_received;
+  if (number_received == 1) {// En verde
+    posicion_actual += 1;
+    contador_tiempos[semaforo_cruzado] = tiempo;
+    semaforo_cruzado += 1;
+  }
 };
-*/
 
 int main(int argc, char const *argv[]){
+
+  printf("I'm the REPARTIDOR process and my PID is: %i ---------------------------------------\n", getpid());
 
   int distancia_semaforo1 = atoi(argv[0]);
   int distancia_semaforo2 = atoi(argv[1]);
   int distancia_semaforo3 = atoi(argv[2]);
   int distancia_bodega = atoi(argv[3]);
-  
+
   // Validando valores de variables
   // printf("distancia_semaforo1: %d\n", distancia_semaforo1);
   // printf("distancia_semaforo2: %d\n", distancia_semaforo2);
   // printf("distancia_semaforo3: %d\n", distancia_semaforo3);
   // printf("distancia_bodega:    %d\n", distancia_bodega);
-  printf("I'm the REPARTIDOR process and my PID is: %i ---------------------------------------\n", getpid());
 
   // Creamos el camino (array) a recorrer por el repartidor
+  int* camino = calloc(distancia_bodega, sizeof(int));
+  for (int i = 0; i <= distancia_bodega; i++){
+    if ((i == distancia_semaforo1) || (i == distancia_semaforo2) || (i == distancia_semaforo3)) {
+      camino[i] = 1;
+    } else if (i == distancia_bodega){
+      camino[i] = 2;
+    } else {
+      camino[i] = 0;
+    };
+  };
   
+  // printeamos el camino
+  
+  // for(int loop = 0; loop < distancia_bodega+1; loop++){
+  //   printf("%d-", camino[loop]);
+  // };
+  while (1) {
+    printf("REPARTIDOR %d_____Estoy en la posicion => %d \n", getpid(), posicion_actual);
+    sleep(10);
+    tiempo += 1;
+    
+    // hay semaforo en siguiente posicion
+    if (camino[posicion_actual + 1] == 1) {
+      printf("Hay un semaforo \n");
+      int pid_personal = getpid();
+      int pid_parent = getppid();
+      int solicitud_envio = pid_personal * 100 + 20 + semaforo_cruzado;
+      posicion_actual +=1; 
+      // preguntar estado de la luz
+      // send_signal_with_int(pid_parent, solicitud_envio);
+      // recibir estado de la luz
+      //wait señal****
+      // connect_sigaction(SIGUSR1, luz_semaforo);
+    
+    // no hay semaforo ni bodega
+    } else if (camino[posicion_actual + 1] == 0){ 
+      printf("Esta vacio...\n");
+      posicion_actual +=1; 
 
-  //sleep(1);
-  exit (0);
-  return 0;
-/*
-  int posicion_actual = 0;
-  int semaforo_cruzado = 0;
-  int contador_tiempos[4] = {0, 0, 0, 0}
-  int tiempo = 0
-
-  while (true){
-    sleep(1);
-    tiempo += 1;
-    // hay semaforo en siguiente posicion
-    if (camino[posicion_actual + 1] = 1){
-      //enviar señal a fábrica consultando estado send_signal_with_int (PID + 20 + semaforo_cruzado);
-      //wait señal
-      //cruzar = recibir señal de fábrica connect_sigaction(SIGUSR1, luz_semaforo);
-      if (cruzar){
-        posicion_actual +=1;
-        semaforo_cruzado +=1;
-        contador_tiempos[semaforo_cruzado] = tiempo;
-      };
-    };
-
-    // no hay semaforo ni bodega
-    else if (camino[posicion_actual + 1] = 0){
-      posicion_actual +=1;
-    }
-
-    // hay bodega
-    else if (camino[posicion_actual + 1] = 2){
-      posicion_actual += 1;
-      sleep(1);
-      tiempo += 1;
-      contador_tiempos[3] = tiempo;
-      break;
-    };
+    // hay bodega
+    } else if (camino[posicion_actual + 1] == 2){
+      printf("LLEGUE A LA BODEGAAA!!!\n");
+      posicion_actual += 1;
+      sleep(1);
+      tiempo += 1;
+      contador_tiempos[3] = tiempo;
+      break;
+      
+    } else {
+      printf("Esto no deberia pasar en repartidor");
+    }
   };
 
   // Generar archivo
-  // Avisar a fábrica el término --> exit(0)
-
-  //LIBERAR CAMINO (FREE)
-  
-*/
+  // Avisar a fábrica el término --> kill(PID,señal)
+  free(camino);
+  exit (0);
+  return 0;
 }
